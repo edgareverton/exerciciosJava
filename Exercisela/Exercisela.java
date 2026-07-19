d5441290bfeb872f8a5e1e0ee8e5a46261dd5a0b
Edgar Everton <edgareverton@outlook.com>
2026-07-18 22:44:47 -0300

    .

---
+++ Exercisela/Exercisela.java
@@ -0,0 +1,14 @@
+class Exercisela {
+    public static void main(String[] args) {
+	 int x = 1;
+	 while (x < 10) {
+    	    if (x > 3) {
+		x = x - 1;
+		System.out.println("Big x");
+		
+
+	
+      }
+    }
+  }
+}
---
+++ test0/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+public static void main(String[] args) {
+	String[] li = {"algo", "aleatório", "baka"};
+	String pa = "Olá";
+	System.out.println(li[0]);
+	System.out.println(pa);
+	
+}
+
+}
