0467438637e238da7af6d4b071c667a087a60bce
Edgar Everton <edgareverton@outlook.com>
2026-07-22 18:49:40 -0300

    06:49 07/22/26

---
+++ projetos_eclipse/entradadaDeDados/src/Scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Scanner {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String x;
+		x = sc.next();
+		
+		System.out.println("Você digitou " + x);
+		sc.close();
+	}
+
+}
--- projetos_eclipse/qualcular_area/src/qualcularArea.java
+++ projetos_eclipse/qualcular_area/src/qualcularArea.java
@@ -2,10 +2,12 @@
 public class qualcularArea {
 	
 	public static void main(String[] args) {
+		
+		//calcula a área
 		double a, b, h, area;
-		a = 6;
-		b = 8;
-		h = 5;
+		a = 6.0;
+		b = 8.0;
+		h = 5.0;
 		
 		area = (a + b) / 2 * h;
 		
