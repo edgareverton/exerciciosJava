62b099b768f6bfad90006cee9d5b5feeacb1dcae
Edgar Everton <edgareverton@outlook.com>
2026-07-22 23:09:07 -0300

    entrada de dados

--- projetos_eclipse/te/src/te.java
+++ projetos_eclipse/te/src/te.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 
 public class te {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String x;
-		x = sc.next();
 		
-		System.out.println("Você digitou " + x);
+		//armazena os dados digitados nas variáveis
+		Scanner sc = new Scanner(System.in);
+		
+		String s1;
+		String s2;
+		String s3;
+		
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("Você digitou.");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
 		sc.close();
+
 	}
 
 }
